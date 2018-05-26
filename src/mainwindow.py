769a613e0b9fc0f466fe3913c4ca062db8fbebc0
Samuel Hoffman <samuelhoffman2@gmail.com>
2018-05-26 15:59:59 -0500

    version 0.0.2

--- src/mainwindow.py
+++ src/mainwindow.py
@@ -24,7 +24,7 @@ from .ui.mainwindow import Ui_MainWindow
 
 HEADERS = ("ID", "Disease Code", "Disease Description", "Patient Count")
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     """Program Main Window."""
